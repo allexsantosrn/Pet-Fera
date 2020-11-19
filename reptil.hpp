84999ea0decde2cb7f64abbd37947afba041a968
irineuanacleto <57273853+irineuanacleto@users.noreply.github.com>
2020-11-19 20:56:12 -0300

    Implementação da classe Reptil

---
+++ reptil.hpp
@@ -0,0 +1,43 @@
+#ifndef _REPTIL_
+#define _REPTIL_
+
+#include "animal.hpp"
+
+#include <string>
+#include <ostream>
+#include <istream>
+
+	class Reptil : public Animal{
+
+		protected:
+			
+			//Marinho ou terrestre
+			char habitat
+
+
+		public: 
+
+			Reptil();
+
+			Reptil(int _id, std::string _classe,std::string _nome,std::string _cientifico
+				char _sexo, Veterinario _veterinario, Tratador _tratador,char _habitat);
+			
+			~Reptil();
+
+			void setHabitat(char _habitat);
+
+			char getHabitat();
+
+		friend std::ostream& operator << (std::ostream &o,Reptil r);
+
+		friend std::istream& operator >> (std::istream &i,Reptil &r);
+
+
+
+
+
+		
+
+			
+
+
