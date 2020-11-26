18f86c46e0ab697d485ee8dc1464804eb77bd090
Esdras Israel <israelferino@gmail.com>
2020-11-26 11:32:04 -0300

    Atualizacoes comentarios exotico.cpp

--- exotico.cpp
+++ exotico.cpp
@@ -1,22 +1,35 @@
+/** 
+ * @file	exotico.cpp
+ * @brief	Definicao da Exotico.
+ * @author	Alexandre Dantas dos Santos e Esdras Israel Ferino dos Santos.
+ * @since	12/11/2020
+ * @date 	24/11/2020
+ */
+
 #include "exotico.hpp"
 
 namespace PetFera {
 
-	// Construtor padrao
+	/** @brief Construtor padrão */ 
 	Exotico::Exotico() {}
 
+	/**
+	 * @param _numibama Numero referente ao Ibama
+	 * @param _regiaoorigem Pais de origem do animal
+	 */	
 	
 	Exotico::Exotico(std::string _numibama, std::string _paisorigem): Silvestre(_numibama) {
 					setpaisOrigem(_paisorigem);
 	}
 
-	// Destrutor padrao
+	/** @brief Destrutor padrão */ 
 	Exotico::~Exotico() {}
 
+	/** @return Retorna o pais de origem do animal*/
 	std::string Exotico::getpaisOrigem() {
 		return this->paisorigem;
 	}
-
+	/** @return Atualiza o pais de origem do animal*/
 	void Exotico::setpaisOrigem(std::string _paisorigem) {
 		this->paisorigem = _paisorigem;
 	}
