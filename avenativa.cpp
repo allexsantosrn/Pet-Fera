9c48d0cfcdce553d369bb5fd19b1d0a1149fed03
Esdras Israel <israelferino@gmail.com>
2020-11-26 11:25:47 -0300

    Atualizacoes comentarios avenativa.hpp e avenativa.cpp

--- avenativa.cpp
+++ avenativa.cpp
@@ -1,11 +1,33 @@
+/** 
+ * @file	aveenativa.cpp
+ * @brief	Definicao da classe Ave Nativa.
+ * @author	Alexandre Dantas dos Santos e Esdras Israel Ferino dos Santos.
+ * @since	12/11/2020
+ * @date 	24/11/2020
+ */
+
 #include "avenativa.hpp"
 
 
 namespace PetFera {
 
-// Contrutor padrao
+	/** @brief Construtor padrão */ 
 	AveNativa::AveNativa() {}
 
+	/**
+	 * @param _id Identificador do animal
+	 * @param _classe Classe do animal
+	 * @param _nome Nome do animal
+	 * @param _cientifico Nome cientifico do animal
+	 * @param _sexo Sexo do animal
+	 * @param _veterinario Veterinario do animal
+	 * @param _tratador Tratador do animal
+	 * @param _venenoso Indica se o animal é venenoso
+	 * @param _habitat Indica o habitat do animal
+	 * @param _tambico Tamanho do bico
+	 * @param _numibama Numero do animal referente ao Ibama
+	 * @param _regiaoorigem Regiao de origem do animal
+	 */	
 	
 	AveNativa::AveNativa(int _id, std::string _classe, std::string _nome, std::string _cientifico, 
 									char _sexo, Veterinario _veterinario, Tratador _tratador, bool _venenoso, std::string _habitat, int _tambico, 
@@ -13,10 +35,10 @@ namespace PetFera {
 									Ave(_id, _classe, _nome, _cientifico,_sexo,_veterinario,_tratador,_venenoso,_habitat,_tambico), 
 									Nativo(_numibama, _regiaoorigem) {}
 
-	// Destrutor padrao
+	/** @brief Destrutor padrão */ 
 	AveNativa::~AveNativa() {}
 
-	
+	/** @return Método para stream de saida */
 	std::ostream& 
 	AveNativa::print(std::ostream &o) {
 		o << "Identificador: " << id << std::endl;
--- avenativa.hpp
+++ avenativa.hpp
@@ -1,3 +1,11 @@
+/** 
+ * @file	aveenativa.hpp
+ * @brief	Definicao da classe Ave Nativa.
+ * @author	Alexandre Dantas dos Santos e Esdras Israel Ferino dos Santos.
+ * @since	12/11/2020
+ * @date 	24/11/2020
+ */
+
 #ifndef _AVENATIVA_
 #define _AVENATIVA_
 
@@ -10,24 +18,31 @@
 
 namespace PetFera {
 
+	/**
+ 	 * @class 	avenativa.hpp
+  	 * @brief	Classe que representa uma Ave Nativa
+  	 * @details Os atributos de uma ave nativa são: id, classe,
+  	 *			nome, cientifico, sexo, veterinario, tratador, 
+  	 *			venenoso, habitat, tamanho do bico, numero ibama e regiao de origem.
+ 	 */
 
 	class AveNativa : public Ave, public Nativo {
 
 		public:
-			 
+			/** @brief Construtor padrão */ 
 			AveNativa();
 
-			 
+			/** @brief Construtor parametrizado */
 			AveNativa(int _id, std::string _classe, std::string _nome, std::string _cientifico,
 			char _sexo, Veterinario _veterinario, Tratador _tratador, bool _venenoso, std::string _habitat, int _tambico, 
 			std::string _ibama, std::string _regiaoorigem);
 
-			 
+			/** @brief Destrutor padrão */ 
 			~AveNativa();
 
 		private:
 
-			 
+			/** @brief Método para imprimir os dados de uma ave nativa*/
 			std::ostream& print(std::ostream&);
 
 
