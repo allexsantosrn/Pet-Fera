90650c8bd6f6215d6c8761e2b69ae1a785d212ea
irineuanacleto <irineuanacleto@ufrn.edu.br>
2020-11-25 22:30:43 -0300

    Atualizando comentarios das classes

--- reptilexotico.cpp
+++ reptilexotico.cpp
@@ -1,11 +1,32 @@
+/** 
+ * @file	reptilexotico.cpp
+ * @brief	Definicao da classe Reptil exótico.
+ * @author	Alexandre Dantas dos Santos e Irineu Anacleto de Lira Neto.
+ * @since	12/11/2020
+ * @date 	24/11/2020
+ */
+
 #include "reptilexotico.hpp"
 
 
 namespace PetFera {
 
-	// Construtor padrao
+	/** @brief Construtor padrão */ 
 	ReptilExotico::ReptilExotico() {}
-
+	/**
+	 * @param _id Identificador do animal
+	 * @param _classe Classe do animal
+	 * @param _nome Nome do animal
+	 * @param _cientifico Nome cientifico do animal
+	 * @param _sexo Sexo do animal
+	 * @param _veterinario Veterinario do animal
+	 * @param _tratador Tratador do animal
+	 * @param _venenoso Indica se o animal é venenoso
+	 * @param _habitar Indica o habitat do animal
+	 * @param _tipopele tipo da pele do animal
+	 * @param _numibama Numero referente ao Ibama
+	 * @param _paisorigem país de origem do animal
+	 */	
 	
 	ReptilExotico::ReptilExotico(int _id, std::string _classe, std::string _nome, std::string _cientifico,
 									char _sexo, Veterinario _veterinario, Tratador _tratador, bool _venenoso, std::string _habitat, std::string _tipopele, 
@@ -13,10 +34,10 @@ namespace PetFera {
 									Reptil(_id, _classe, _nome, _cientifico,_sexo,_veterinario,_tratador,_venenoso,_habitat,_tipopele),
 									Exotico(_numibama,_paisorigem) {}
 
-	// Destrutor padrao
+	/** @brief Destrutor padrão */ 
 	ReptilExotico::~ReptilExotico() {}
 
-	
+	/** @return Método para stream de saída; */
 	std::ostream& 
 	ReptilExotico::print(std::ostream &o) {
 		o << "Identificador: " << id << std::endl;
--- reptilexotico.hpp
+++ reptilexotico.hpp
@@ -1,3 +1,13 @@
+/** 
+ * @file	reptilexotico.hpp
+ * @brief	Definicao das classe reptil exótico.
+ * @author	Alexandre Dantas dos Santos e Irineu Anacleto de Lira Neto.
+ * @since	12/11/2020
+ * @date 	24/11/2020
+ */	
+
+
+
 #ifndef _REPTILEXOTICO_
 #define _REPTILEXOTICO_
 
@@ -10,23 +20,33 @@
 
 namespace PetFera {
 	
+	/**
+ 	 * @class 	reptilexotico.hpp
+  	 * @brief	Classe que representa um reptil exotico
+  	 * @details Os atributos de um reptil exotico são: id, classe,
+  	 *			nome, cientifico, sexo, veterinario, tratador, 
+  	 *			venenoso, habitat, tipopele, numero ibama e país de origem.
+ 	 */
+	
+	
 	class ReptilExotico : public Reptil, public Exotico {
 
 		public:
-			 
+			
+			/** @brief Construtor padrão */  
 			ReptilExotico();
 
-			 
+			/** @brief Construtor parametrizado */ 
 			ReptilExotico(int _id, std::string _classe, std::string _nome, std::string _cientifico,
 			char _sexo, Veterinario _veterinario, Tratador _tratador, bool _venenoso, std::string _habitat, std::string _tipopele,
 			std::string _numibama, std::string _paisorigem);
 
-			 
+			/** @brief Destrutor padrão */  
 			~ReptilExotico();
 
 		private:
 
-			 
+			/** @brief Método para imprimir os dados de um Reptil exotico*/  
 			std::ostream& print(std::ostream&);
 
 
