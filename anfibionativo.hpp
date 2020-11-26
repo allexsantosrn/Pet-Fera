3d3528a33bafe74e3e33aae0f7652bd0e1902375
Esdras Israel <israelferino@gmail.com>
2020-11-25 21:27:32 -0300

    Anfibio Nativo

--- anfibionativo.hpp
+++ anfibionativo.hpp
@@ -1,3 +1,11 @@
+/** 
+ * @file	anfibio.hpp
+ * @brief	Definicao da classe Anfibio Exotico.
+ * @author	Esdras Israel Ferino dos Santos.
+ * @since	12/11/2020
+ * @date 	24/11/2020
+ */	
+
 #ifndef _ANFIBIONATIVO_
 #define _ANFIBIONATIVO_
 
@@ -9,24 +17,32 @@
 
 namespace PetFera {
 	
+	/**
+ 	 * @class 	anfibioexotico.hpp
+  	 * @brief	Classe que representa um Anfibio Exotico
+  	 * @details Os atributos de um anfibio exotico são: id, classe,
+  	 *			nome, cientifico, sexo, veterinario, tratador, 
+  	 *			venenoso, habitat, qtdmudas, numero ibama e regiao de origem.
+ 	 */
 	
 	class AnfibioNativo : public Anfibio, public Nativo {
 
 		public:
-			 
+			
+			/** @brief Construtor padrão */ 
 			AnfibioNativo();
 
-			 
+			/** @brief Construtor parametrizado */
 			AnfibioNativo(int _id, std::string _classe, std::string _nome, std::string _cientifico,
 			char _sexo, Veterinario _veterinario, Tratador _tratador, bool _venenoso, std::string _habitat, int _qtdmudas, 
 			std::string _ibama, std::string _regiaoorigem);
 
-			 
+			/** @brief Destrutor padrão */ 
 			~AnfibioNativo();
 
 		private:
 
-			 
+			/** @brief Método para imprimir os dados de um anfibio exotico*/ 
 			std::ostream& print(std::ostream&);
 
 
